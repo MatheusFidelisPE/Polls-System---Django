3eacb4d3705444f95b231d9de225966bae6677f4
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2023-02-24 22:57:47 -0300

    :sparkles: Criando novas funções que retornam uma HttpResponse no arquivos polls/views.py. Essas views funcionam apenas para entender o funcionamento de roteamento do Django

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 
+# Os métodos nesse arquivo views.py que são invocados no arquivo polls/urls.py precisam ter como primeiro argumento
+# um argumento chamado request
+
+
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("Você está procurando pela questão %s" % question_id)
+
+
+def results(request, question_id):
+    response = "Você está procurando pelos resultados da questão %s"
+    return HttpResponse(response%question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Você estará vontando na questão %s" % question_id)
+
+
