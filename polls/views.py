9df558508b132a59206196abc39f1c3f5421d1d1
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2023-02-25 21:52:59 -0300

    :sparkles: Construindo as visualizações em arquivos html, retirando as responses feitas inteiramente em python

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
+
 
 # Os métodos nesse arquivo views.py que são invocados no arquivo polls/urls.py precisam ter como primeiro argumento
 # um argumento chamado request
 
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("Você está procurando pela questão %s" % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question Does Not Exist")
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question':question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
